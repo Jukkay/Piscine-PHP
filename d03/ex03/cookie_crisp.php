0b9251e1fb6db9d7684ea4313a584f89f7f8d0d2
Jukka Ylimaula <jylimaul@student.hive.fi>
2022-03-20 11:34:40 +0200

    d03 ex03

--- d03/ex03/cookie_crisp.php
+++ d03/ex03/cookie_crisp.php
@@ -1,2 +1,14 @@
 <?php
+switch ($_GET["action"]) {
+	case "set":
+		setcookie($_GET["name"], $_GET["value"], time() + 86400, "/");
+		break;
+	case "get":
+		if ($_COOKIE[$_GET["name"]])
+			echo $_COOKIE[$_GET["name"]] . "\n";
+		break;
+	case "del":
+		setcookie($_GET["name"], null, -1, "/");
+		break;
+}
 ?>
